357c6c67894dbde75b5f6008feb0d40a8496d867
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-06 00:54:29 +0200

    else if fixed

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,16 +20,19 @@ public class Main {
 			System.out.println("Inserisci il tipo di file del " + (i + 1) + "° elemento (video, immagine o audio)");
 			String mediaType = multimediaScan.nextLine();
 
-			System.out.println("Inserisci il titolo del " + (i + 1) + "° elemento");
-			String mediaTitle = multimediaScan.nextLine();
-
 			if (mediaType.equalsIgnoreCase("video")) {
+				System.out.println("Inserisci il titolo del " + (i + 1) + "° video");
+				String mediaTitle = multimediaScan.nextLine();
 				Video video = new Video(mediaType, mediaTitle);
 				medias[i] = video;
 			} else if (mediaType.equalsIgnoreCase("audio")) {
+				System.out.println("Inserisci il titolo del " + (i + 1) + "° audio");
+				String mediaTitle = multimediaScan.nextLine();
 				Audio audio = new Audio(mediaType, mediaTitle);
 				medias[i] = audio;
 			} else if (mediaType.equalsIgnoreCase("immagine")) {
+				System.out.println("Inserisci il titolo della " + (i + 1) + "° immagine");
+				String mediaTitle = multimediaScan.nextLine();
 				Immagine immagine = new Immagine(mediaType, mediaTitle);
 				medias[i] = immagine;
 			} else {
