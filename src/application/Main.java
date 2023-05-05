1e09ee92ac25fab33c9ef73cdc3773748ac401b9
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-05 23:35:36 +0200

    deleted comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,18 +10,7 @@ import application.media.Video;
 public class Main {
 
 	public static void main(String[] args) {
-//		Immagine imm1 = new Immagine("immagine", "nature", 10);
-//		Luminosita.show(imm1.getTitolo(), imm1.getLuminosita());
-//
-//		Audio song = new Audio("audio", "queen", 5);
-//
-//		song.setVolume(30);
-//		Riproducibile.play(song.getDurata(), song.getTitolo(), song.getVolume());
-//
-//		Video video1 = new Video("video", "avatar", 10, 20);
-//
-//		Riproducibile.play(video1.getDurata(), video1.getTitolo(), video1.getVolume(),
-//				Luminosita.show(video1.getLuminosita()));
+
 		Scanner multimediaScan = new Scanner(System.in);
 		System.out.println("Inserisci 5 elementi multimediali");
 
@@ -69,27 +58,7 @@ public class Main {
 			}
 		}
 		System.out.println("Arrivederci!");
+		multimediaScan.close();
 	}
 
-	/*
-	 * scanner
-	 * println("inserisci un elemento multimediale specificando tipo di file e titolo"
-	 * )
-	 * 
-	 * println("inserisci il tipo del primo file")
-	 * 
-	 * string file1 = scanner.nextLine;
-	 * println("inserisci il titolo del primo file")
-	 * 
-	 * string titolo1 = scanner.nextLine; ElementoMultimediale[] media = new
-	 * ElementoMultimediale[5] if (file1.equals("video")){ Video video1 = new
-	 * Video(file, titolo) }else if (file1.equals("audio")){ Audio audio1 = new
-	 * Audio(file, titolo) }else if (file1.equals("immagine")){ Immagine imm1 = new
-	 * Immagine (file, titolo) }else{ println("file non supp") }
-	 * 
-	 * media[i] = file; scanner.close() ciclo chiuso****
-	 * println("Inserisci un numero da 1 a 5 per riprodurre uno dei file inseriti"
-	 * 
-	 * 
-	 */
 }
